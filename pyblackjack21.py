308e47729efc5286e4c65185e1dd865e7fd3a9d2
Jay2995 <josey02995@gmail.com>
2023-10-07 03:02:30 +0300

    finished game

--- pyblackjack21.py
+++ pyblackjack21.py
@@ -1,41 +1,82 @@
-import random;
-import art
-import os
+import random
 import sys
+import os
+import art
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
-
-user_cards = [];
-dealer_cards = [];
-user_score = 0;
-dealer_score = 0;
-
-#functions needed
-
-#random cards generator && score calculator:
-
-def random_cards (target_list):
-    for _ in range(2):
-        random_index = random.randrange(len(cards))
-        random_item = cards[random_index]
-        target_list.append(random_item);
-
-    score = sum(target_list);         
-    return score;
+# returns random card from deck
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+    card = random.choice(cards);
+    return card
 
 
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0;
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11);
+        cards.append(1);
+    return sum(cards);
 
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "Lose, opponent has blackjack";
+    elif user_score == 0:
+        return "Win with Blackjack";
+    elif user_score > 21:
+        return "You went over, you lose"
+    elif computer_score > 21:
+        return "Opponent went over, you win";
+    elif user_score > computer_score:
+        return "You win";
+    else:
+        return "You lose";
 
-start_msg = input("Do you want to play a game of blackjack? y or n: ").lower();
-if start_msg == "y":
-    os.system('cls');
+def blackjack():
     print(art.logo);
-    user_score = random_cards(user_cards);
-    dealer_score = random_cards(dealer_cards);
-    print(f"Your cards: {user_cards}, current score: {user_score} ")
-else:
-    print("Exiting...")
-    sys.exit()
+    user_cards = [];
+    computer_cards = [];
+    is_game_over = False;
 
-    
+    for _ in range(2):
+        user_cards.append(deal_card());
+        computer_cards.append(deal_card());
 
+    while not is_game_over:
+        user_score = calculate_score(user_cards);
+        computer_score = calculate_score(computer_cards);
+
+        print(f"Your cards: {user_cards}, current score: {user_score}");
+        print(f"computer first card: {computer_cards[0]}");
+
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ").lower();
+            if user_should_deal == "y":
+                user_cards.append(deal_card());
+            else:
+                is_game_over = True
+
+    while computer_score !=0  and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"Your final hand: {user_cards}, final score {user_score}");
+    print(f"computer's final hand: {computer_cards}, final score {computer_score}");
+
+    print(compare(user_score, computer_score));
+
+    play_again = input("Do you want to play another game of blackjack?: ").lower();
+    if play_again == "y":
+        os.system('cls');
+        blackjack();
+    else:
+        print("Exiting game");
+        sys.exit();
+
+
+blackjack();
